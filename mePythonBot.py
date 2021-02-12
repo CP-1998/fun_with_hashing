e858fcb6af822e02ec78063bba2fd367456859a4
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-02-12 11:27:38 -0800

    first commit

---
+++ mePythonBot.py
@@ -0,0 +1,21 @@
+import random
+import hashlib
+
+hashList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+i = 0
+hashChoice = str()
+while i < 6:
+  hashChoice += random.choice(hashList)
+  i += 1
+hashInterim = hashChoice.encode('utf-8')
+hashFunc = hashlib.sha256()
+hashFunc.update(hashInterim)
+hashFinal = hashFunc.hexdigest()
+
+  
+
+print(hashFinal)
+
