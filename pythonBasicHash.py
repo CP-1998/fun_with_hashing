44899a1fa10f378f3872a6711d1474d49a552c2c
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-02-12 11:40:30 -0800

    placed the hashing actions inside the 'hashFunction' function

--- pythonBasicHash.py
+++ pythonBasicHash.py
@@ -1,21 +1,20 @@
 import random
 import hashlib
 
-hashList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+
+def hashFunction():
+  hashList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+            
+  i = 0
+  hashChoice = str()
+  while i < 10:
+    hashChoice += random.choice(hashList)
+    i += 1
+  hashInterim = hashChoice.encode('utf-8')
+  hashFunc = hashlib.sha256()
+  hashFunc.update(hashInterim)
+  hashFinal = hashFunc.hexdigest()
+  print(hashFinal)
 
-
-i = 0
-hashChoice = str()
-while i < 10:
-  hashChoice += random.choice(hashList)
-  i += 1
-hashInterim = hashChoice.encode('utf-8')
-hashFunc = hashlib.sha256()
-hashFunc.update(hashInterim)
-hashFinal = hashFunc.hexdigest()
-
-  
-
-print(hashFinal)
-
+hashFunction()
