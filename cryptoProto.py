b119c8663464534d73b974d3fda22ef38cc40676
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-02-13 17:23:32 -0800

    swapped random for secret, as random is not a secure random method. Expanded hashList *createHash() to include upper case and special characters

--- cryptoProto.py
+++ cryptoProto.py
@@ -1,6 +1,6 @@
 import hashlib
-import random
 import datetime
+import secrets
 
 
 # The SourceBloc class is the blueprint for the origin block, and all preceding blocks.
@@ -14,21 +14,24 @@ class SourceBloc:
 
 # The createHash function will develop a unique hash for each block in the chain
   def createHash(self):
-    hashList = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-                'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    hashList = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J',
+                'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',
+                'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
+                '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
     i = 0
     hashChoice = str()
-    while i < 10:
-      hashChoice += random.choice(hashList)
+    while i < 256:
+      hashChoice += secrets.choice(hashList)
       i += 1
+    print(hashChoice)
     hashInterim = hashChoice.encode('utf-8')
     hashFunc = hashlib.sha256()
     hashFunc.update(hashInterim)
     hashFinal = hashFunc.hexdigest()
     return (hashFinal)
 
-# Creates a unique timestamp at compilation to be attaced to the bloc object for verification
+# Creates a unique timestamp at compilation to be attached to the bloc object for verification
   def createTimestamp(self):
     today = datetime.datetime.now()
     return(today.strftime("%Y-%m-%d %H:%M:%S"))
@@ -41,21 +44,27 @@ class SourceBloc:
 
 # Develops unique number for PoW mining, aka nonce
   def createNonce(self):
-    num_list = [1, 1.5, 2, 2.2, 3, 3.7, 4, 4.1, 5, 5.2, 6, 6.6, 7, 7.9, 8, 8.3, 9, 9.5, 0, 0.5]
+    num_list = [ 1.5, 2.2, 3.7, 4.1, 5.2, 6, 6.6, 7, 7.9, 8.3, 9.5, 0.5]
 
     i = 0
     hashChoice = int()
     while i < 5:
-      hashChoice += random.choice(num_list)
+      hashChoice += secrets.choice(num_list)
       i += 1
-    finalNonce = hashChoice * 2334 / 2
+    finalNonce = hashChoice * 2334
     return (finalNonce)
 
 
-# Defines the source of the chain, not sure exactly if what I'm doing here will work but
+# Defines the source of the chain, not sure exactly if what I'm doing here will work but...
   def homeFunction(self):
     home = 0
     return home
+
+
+
+# A new instance of SourceBloc is being created to play around with. I eventually will create a self.function to build new SourceBloc
+# objects, which will inherit the SourceBloc blueprint but will add the previousHash function to verify the legitimacy of the sent
+# data. I have a lot to learn before proceeding to this point however.
 
 bloc = SourceBloc('', '', '', '', '')
 
