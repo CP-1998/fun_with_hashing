b5b0be5072b7f3d1783afeb664a3a127cc056e68
thedogz11 <69052651+thedogz11@users.noreply.github.com>
2021-02-13 17:30:08 -0800

    added duplicate lowercase letters to increase randomness

--- cryptoProto.py
+++ cryptoProto.py
@@ -14,8 +14,8 @@ class SourceBloc:
 
 # The createHash function will develop a unique hash for each block in the chain
   def createHash(self):
-    hashList = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J',
-                'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',
+    hashList = ['a', 'a', 'A', 'b', 'b', 'B', 'c', 'c', 'C', 'd', 'd', 'D', 'e', 'e', 'E', 'f', 'f', 'F', 'g', 'g', 'G', 'h', 'h' 'H', 'i', 'i' 'I', 'j', 'j', 'J',
+                'k', 'k', 'K', 'l', 'l', 'L', 'm', 'm', 'M', 'n', 'n', 'N', 'o', 'o' 'O', 'p', 'p' 'P', 'q', 'q', 'Q', 'r', 'r', 'R', 's', 's', 'S', 't', 't', 'T',
                 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z', '0', '1', '2', '3', '4', '5', '6', '7',
                 '8', '9', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
